ea5201a3c1675de42e9db56a39e2732829a91333
axie <axie@NZAX01AKL>
2016-09-23 12:29:33 +1200

    Add Kill connection

---
+++ KillAllDbConnections.ps1
@@ -0,0 +1,17 @@
+﻿param ($server, $db)
+
+
+function KillDBConnections ($srvName, $DBName)
+{
+	
+    import-module SQLPS -DisableNameChecking
+	$srv = New-Object -TypeName 'Microsoft.SqlServer.Management.Smo.Server' -ArgumentList $srvName
+    $srv.KillAllProcesses($DBName)
+    #Write-Host $srvName
+
+}
+
+
+KillDBConnections -srvName $server -DBName $db
+Pop-Location 
+
